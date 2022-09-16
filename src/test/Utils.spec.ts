1fc52021e9220f0d0e4b3a494c190c5278a18024
marcos <marcos.cosmoski@gmail.com>
2022-09-16 16:40:41 +0100

    jest tests

---
+++ src/test/Utils.spec.ts
@@ -0,0 +1,60 @@
+import { Utils } from '../app/Utils'
+
+describe('Utils test suite', () => { 
+
+  // beforeEach(() => {
+    
+  // })
+
+  // beforeAll(() => { 
+
+  // })
+
+  test('first test', () => { 
+    const result = Utils.toUpperCase('abc')
+    expect(result).toBe("ABC");
+  })
+
+  test('parse simple URL', () => { 
+    const parsedUrl = Utils.parseUrl("http://localhost:8080/login");
+    expect(parsedUrl.href).toBe("http://localhost:8080/login");
+    expect(parsedUrl.port).toBe('8080')
+    expect(parsedUrl.protocol).toBe('http:');
+    expect(parsedUrl.query).toEqual({});
+  })
+
+  test('parse URL with query', () => { 
+    const parsedUrl = Utils.parseUrl("http://localhost:8080/login?user=user&password=pass");
+    const expectedQuery = { 
+      user: 'user',
+      password: 'pass'
+    }
+    expect(parsedUrl.query).toEqual(expectedQuery);
+    expect(expectedQuery).toBe(expectedQuery)
+  })
+
+  test('test invalid URL', () => { 
+    function expectedError() { 
+      Utils.parseUrl('')
+    }
+    expect(expectedError).toThrowError();
+    expect(expectedError).toThrow('Empty url')
+  })
+
+  test('test invalid URL with arrow function', () => { 
+    expect(() => { 
+      Utils.parseUrl('')
+    }).toThrow('Empty url')
+  })
+
+  test('test invalid URL with try catch', () => { 
+      try { 
+        Utils.parseUrl('')
+      } catch (error) { 
+        expect(error).toBeInstanceOf(Error);
+        expect(error).toHaveProperty('message', 'Empty url')
+      }
+      
+    
+  })
+})
